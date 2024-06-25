059a5c2f4fa192d2319a0323df77f7183dd0741f
htmos6 <mehmet.arslan4000@gmail.com>
2024-06-25 16:03:59 +0300

    system files added

--- FullAdder/FullAdder.runs/synth_1/NbitFullAdder.tcl
+++ FullAdder/FullAdder.runs/synth_1/NbitFullAdder.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "C:/Users/Legion/Desktop/Quartus/VHDL/VHDL-Tutorials/FullAdder/FullAdder.runs/synth_1/FullAdder.tcl"
+  variable script "C:/Users/Legion/Desktop/Quartus/VHDL/VHDL-Tutorials/FullAdder/FullAdder.runs/synth_1/NbitFullAdder.tcl"
   variable category "vivado_synth"
 }
 
@@ -72,8 +72,9 @@ set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
 read_vhdl -library xil_defaultlib {
-  C:/Users/Legion/Desktop/Quartus/VHDL/VHDL-Tutorials/FullAdder/FullAdder.srcs/sources_1/new/HalfAdder.vhd
   C:/Users/Legion/Desktop/Quartus/VHDL/VHDL-Tutorials/FullAdder/FullAdder.srcs/sources_1/new/FullAdder.vhd
+  C:/Users/Legion/Desktop/Quartus/VHDL/VHDL-Tutorials/FullAdder/FullAdder.srcs/sources_1/new/HalfAdder.vhd
+  C:/Users/Legion/Desktop/Quartus/VHDL/VHDL-Tutorials/FullAdder/FullAdder.srcs/sources_1/new/NbitFullAdder.vhd
 }
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -85,10 +86,12 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
 set_param ips.enableIPCacheLiteLoad 1
+
+read_checkpoint -auto_incremental -incremental C:/Users/Legion/Desktop/Quartus/VHDL/VHDL-Tutorials/FullAdder/FullAdder.srcs/utils_1/imports/synth_1/FullAdder.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
-synth_design -top FullAdder -part xczu7ev-ffvc1156-2-e
+synth_design -top NbitFullAdder -part xczu7ev-ffvc1156-2-e
 OPTRACE "synth_design" END { }
 if { [get_msg_config -count -severity {CRITICAL WARNING}] > 0 } {
  send_msg_id runtcl-6 info "Synthesis results are not added to the cache due to CRITICAL_WARNING"
@@ -98,10 +101,10 @@ if { [get_msg_config -count -severity {CRITICAL WARNING}] > 0 } {
 OPTRACE "write_checkpoint" START { CHECKPOINT }
 # disable binary constraint mode for synth run checkpoints
 set_param constraints.enableBinaryConstraints false
-write_checkpoint -force -noxdef FullAdder.dcp
+write_checkpoint -force -noxdef NbitFullAdder.dcp
 OPTRACE "write_checkpoint" END { }
 OPTRACE "synth reports" START { REPORT }
-generate_parallel_reports -reports { "report_utilization -file FullAdder_utilization_synth.rpt -pb FullAdder_utilization_synth.pb"  } 
+generate_parallel_reports -reports { "report_utilization -file NbitFullAdder_utilization_synth.rpt -pb NbitFullAdder_utilization_synth.pb"  } 
 OPTRACE "synth reports" END { }
 file delete __synthesis_is_running__
 close [open __synthesis_is_complete__ w]
